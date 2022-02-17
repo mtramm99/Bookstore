1f9ef02638c26bc9e328f81a7831201e64e113f6
Mark Trammell <mtramm99@gmail.com>
2022-02-17 00:13:54 -0700

    poo

--- Mission7/Infrastructure/PaginationTagHelper.cs
+++ Mission7/Infrastructure/PaginationTagHelper.cs
@@ -30,6 +30,10 @@ namespace Bookstore.Infrastructure
         //Different than View Context
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -41,8 +45,13 @@ namespace Bookstore.Infrastructure
                 TagBuilder tb = new TagBuilder ("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
-
                 final.InnerHtml.AppendHtml(tb);
             }
 
