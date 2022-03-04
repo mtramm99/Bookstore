e8e470d506b0a9233d40e55121acb98093c96ba0
Mark Trammell <mtramm99@gmail.com>
2022-03-03 18:20:39 -0700

    blah

---
+++ Mission7/Controllers/PurchaseController.cs
@@ -0,0 +1,30 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class PurchaseController : Controller
+    {
+
+        public PurchaseController ()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View(new Purchase());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Purchase purchase)
+        {
+
+        }
+    }
+}
